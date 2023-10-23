6ba2eec308e84a33044ebb00a6296ac87b9e10f5
Season Zou <jijiezou@stu.pku.edu.cn>
2023-10-23 20:11:47 +0800

    fix some trivial mistakes in test_negf_hamiltonian.py

--- dptb/tests/test_negf_hamiltonian.py
+++ dptb/tests/test_negf_hamiltonian.py
@@ -53,40 +53,77 @@ def test_negf_Hamiltonian(root_directory):
         struct_device, struct_leads = hamiltonian.initialize(kpoints=kpoints)
     
     #check device's Hamiltonian
-    device_symbol= struct_device.symbols=="C4"
-    device_pbc= struct_device.pbc==False
-    device_cell= struct_device.cell==[10.0, 10.0, 19.2]
-    assert device_symbol.any()==True
-    assert device_pbc.any()==True
-    assert device_cell.any()==True
-
+    assert all(struct_device.symbols=="C4")
+    assert all(struct_device.pbc)==False
+    assert np.diag(np.array(struct_device.cell==[10.0, 10.0, 19.2])).all()
+    
     #check lead_L's Hamiltonian
-    leads_symbol= struct_leads["lead_L"].symbols=="C4"
-    leads_pbc= struct_leads["lead_L"].pbc==False
-    leads_cell= struct_leads["lead_L"].cell==[10.0, 10.0, 6.4]
-    assert leads_symbol.any()==True
-    assert leads_pbc.any()==True
-    assert leads_cell.any()==True  
+ 
+    assert all(struct_leads["lead_L"].symbols=="C4")
+    assert struct_leads["lead_L"].pbc[0]==False
+    assert struct_leads["lead_L"].pbc[1]==False
+    assert struct_leads["lead_L"].pbc[2]==True
+    assert np.diag(np.array(struct_leads["lead_L"].cell==[10.0, 10.0, -6.4])).all()
     
     #check lead_R's Hamiltonian
-    leads_symbol= struct_leads["lead_R"].symbols=="C4"
-    leads_pbc= struct_leads["lead_R"].pbc==False
-    leads_cell= struct_leads["lead_R"].cell==[10.0, 10.0, 6.4]
-    assert leads_symbol.any()==True
-    assert leads_pbc.any()==True
-    assert leads_cell.any()==True 
+ 
+    assert all(struct_leads["lead_R"].symbols=="C4")
+    assert struct_leads["lead_R"].pbc[0]==False
+    assert struct_leads["lead_R"].pbc[1]==False
+    assert struct_leads["lead_R"].pbc[2]==True
+    assert np.diag(np.array(struct_leads["lead_R"].cell==[10.0, 10.0, 6.4])).all()
+
 
     #check hs_device
-    hs_device = hamiltonian.get_hs_device(kpoint=np.array([0,0,0]),V=0,block_tridiagonal=False)[0][0]
-    hs_device_standard = torch.tensor([[-13.6386+0.j,   0.6096+0.j,   0.0000+0.j,   0.0000+0.j],
+    h_device = hamiltonian.get_hs_device(kpoint=np.array([0,0,0]),V=0,block_tridiagonal=False)[0][0]
+    print(hamiltonian.get_hs_device(kpoint=np.array([0,0,0]),V=0,block_tridiagonal=False)[0])
+    h_device_standard = torch.tensor([[-13.6386+0.j,   0.6096+0.j,   0.0000+0.j,   0.0000+0.j],
         [  0.6096+0.j, -13.6386+0.j,   0.6096+0.j,   0.0000+0.j],
         [  0.0000+0.j,   0.6096+0.j, -13.6386+0.j,   0.6096+0.j],
         [  0.0000+0.j,   0.0000+0.j,   0.6096+0.j, -13.6386+0.j]],dtype=torch.complex128)
-    assert abs(hs_device-hs_device_standard).all()<1e-4
+    assert abs(h_device-h_device_standard).max()<1e-4
+
+    s_device = hamiltonian.get_hs_device(kpoint=np.array([0,0,0]),V=0,block_tridiagonal=False)[1][0]
+    print(hamiltonian.get_hs_device(kpoint=np.array([0,0,0]),V=0,block_tridiagonal=False)[1][0])
+    s_standard = torch.eye(4)
+    assert abs(s_device-s_standard).max()<1e-5
+
+
 
     #check hs_lead
-    hs_lead = hamiltonian.get_hs_lead(kpoint=np.array([0,0,0]),tab="lead_L",v=0)[0][0]
-    hs_lead_standard = torch.tensor([-13.6386+0.j,   0.6096+0.j], dtype=torch.complex128)    
-    assert abs(hs_lead-hs_lead_standard).max()<1e-4
+    hl_lead = hamiltonian.get_hs_lead(kpoint=np.array([0,0,0]),tab="lead_L",v=0)[0][0]
+    hl_lead_standard = torch.tensor([-13.6386+0.j,   0.6096+0.j], dtype=torch.complex128)    
+    assert abs(hl_lead-hl_lead_standard).max()<1e-4
+
+    hll_lead = hamiltonian.get_hs_lead(kpoint=np.array([0,0,0]),tab="lead_L",v=0)[1][0]
+    hll_lead_standard = torch.tensor([0.0000+0.j, 0.6096+0.j], dtype=torch.complex128)
+    print(hll_lead)    
+    assert abs(hll_lead-hll_lead_standard).max()<1e-4
+
+    hDL_lead = hamiltonian.get_hs_lead(kpoint=np.array([0,0,0]),tab="lead_L",v=0)[2]
+    hDL_lead_standard = torch.tensor([[0.0000+0.j, 0.6096+0.j],
+        [0.0000+0.j, 0.0000+0.j],
+        [0.0000+0.j, 0.0000+0.j],
+        [0.0000+0.j, 0.0000+0.j]], dtype=torch.complex128)   
+    assert abs(hDL_lead-hDL_lead_standard).max()<1e-5
+
+    sl_lead = hamiltonian.get_hs_lead(kpoint=np.array([0,0,0]),tab="lead_L",v=0)[3]
+    sl_lead_standard = torch.eye(2)   
+    assert abs(sl_lead-sl_lead_standard).max()<1e-5
+
+    sll_lead = hamiltonian.get_hs_lead(kpoint=np.array([0,0,0]),tab="lead_L",v=0)[4]
+    sll_lead_standard = torch.zeros(2)    
+    assert abs(sll_lead-sll_lead_standard).max()<1e-5
+
+    sDL_lead = hamiltonian.get_hs_lead(kpoint=np.array([0,0,0]),tab="lead_L",v=0)[5]
+    sDL_lead_standard = torch.zeros([4,2])    
+    assert abs(sDL_lead-sDL_lead_standard).max()<1e-5   
+
+
+    # check device norbs
+    na = len(hamiltonian.device_norbs)
+    device_norbs_standard=[1,1,1,1]
+    assert na == 4
+    assert hamiltonian.device_norbs==device_norbs_standard
 
    
