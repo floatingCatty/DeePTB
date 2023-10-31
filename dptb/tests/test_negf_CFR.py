532a77391f2bedbc0b490e59273a92fa39a80983
Season Zou <jijiezou@stu.pku.edu.cn>
2023-10-31 19:05:25 +0800

    "improve the accuracy of test_negf_CFR.py"

--- dptb/tests/test_negf_CFR.py
+++ dptb/tests/test_negf_CFR.py
@@ -5,9 +5,9 @@ from dptb.negf.CFR import ozaki_residues
 def test_ozaki():
     
     p, r = ozaki_residues(M_cut=1)
-    assert torch.round(p,decimals=4)==torch.tensor([3.4641], dtype=torch.float64)
-    assert torch.round(r,decimals=4)==torch.tensor([1.5000], dtype=torch.float64)
+    assert abs(p-torch.tensor([3.464101615], dtype=torch.float64))<1e-8
+    assert abs(r-torch.tensor([1.499999999], dtype=torch.float64))<1e-8
     p1, r1 = ozaki_residues(M_cut=2)
     for i in range(2):
-        assert torch.round(p1[i],decimals=4)==torch.tensor([ 3.1425, 13.0432], dtype=torch.float64)[i]
-        assert torch.round(r1[i],decimals=4)==torch.tensor([1.0023, 3.9977], dtype=torch.float64)[i]
+        assert abs(p1[i]-torch.tensor([3.142466786, 13.043193723], dtype=torch.float64)[i])<1e-8
+        assert abs(r1[i]-torch.tensor([1.002338271, 3.997661728], dtype=torch.float64)[i])<1e-8
