0819a9e2eb04c738c42e5efece9ab0e10f06e353
SharpLonde <tintingg@163.com>
2023-12-31 14:42:15 +0800

    Unified dataset IO & added ASE trajectory support

--- dptb/data/dataset/_default_dataset.py
+++ dptb/data/dataset/_default_dataset.py
@@ -1,8 +1,11 @@
 from typing import Dict, Any, List, Callable, Union, Optional
 import os
+import glob
 
 import numpy as np
 import h5py
+from ase import Atoms
+from ase.io import Trajectory
 
 import torch
 
@@ -18,25 +21,33 @@ from dptb.utils.tools import j_loader
 
 class _TrajData(object):
     '''
-    Input file format in a trajectory (shape):
-    "info.json": includes infomation in the data files.
+    Input files format in a trajectory (shape):
+    "info.json": optional, includes infomation in the data files.
+                 can be provided in the base (upper level) folder, or assign in each trajectory.
     "cell.dat": fixed cell (3, 3) or variable cells (nframes, 3, 3). Unit: Angstrom
     "atomic_numbers.dat": (natoms) or (nframes, natoms)
     "positions.dat": concentrate all positions in one file, (nframes * natoms, 3). Can be cart or frac.
 
-    Optional:
+    Optional data files:
     "eigenvalues.npy": concentrate all engenvalues in one file, (nframes, nkpoints, nbands)
     "kpoints.npy": MUST be provided when loading `eigenvalues.npy`, (nkpoints, 3) or (nframes, nkpints, 3)
     "hamiltonians.h5": h5 file storing atom-wise hamiltonian blocks labeled by frames id and `i0_jR_Rx_Ry_Rz`.
     "overlaps.h5": the same format of overlap blocks as `hamiltonians.h5`
     '''
     
-    def __init__(self, root: str, AtomicData_options: Dict[str, Any] = {},):
+    def __init__(self, 
+                 root: str, 
+                 AtomicData_options: Dict[str, Any] = {},
+                 get_Hamiltonian = False,
+                 get_eigenvalues = False,
+                 info = None,
+                 _clear = False):
         self.root = root
         self.AtomicData_options = AtomicData_options
-        self.info = j_loader(os.path.join(root, "info.json"))
+        self.info = info
 
         self.data = {}
+        # load cell
         cell = np.loadtxt(os.path.join(root, "cell.dat"))
         if cell.shape[0] == 3:
             # same cell size, then copy it to all frames.
@@ -46,20 +57,21 @@ class _TrajData(object):
             self.data["cell"] = cell.reshape(self.info["nframes"], 3, 3)
         else:
             raise ValueError("Wrong cell dimensions.")
+        
+        # load atomic numbers
         atomic_numbers = np.loadtxt(os.path.join(root, "atomic_numbers.dat"))
-        if len(atomic_numbers.shape) == 1:
+        if atomic_numbers.shape[0] == self.info["natoms"]:
             # same atomic_numbers, copy it to all frames.
-            if atomic_numbers.shape[0] == self.info["natoms"]:
-                atomic_numbers = np.expand_dims(atomic_numbers, axis=0)
-                self.data["atomic_numbers"] = np.broadcast_to(atomic_numbers, (self.info["nframes"], 
-                                                                               self.info["natoms"]))
-            else:
-                raise ValueError("Atomic numbers not equal to natoms in info.json. ")
+            atomic_numbers = np.expand_dims(atomic_numbers, axis=0)
+            self.data["atomic_numbers"] = np.broadcast_to(atomic_numbers, (self.info["nframes"], 
+                                                                           self.info["natoms"]))
         elif atomic_numbers.shape[0] == self.info["natoms"] * self.info["nframes"]:
             self.data["atomic_numbers"] = atomic_numbers.reshape(self.info["nframes"],
                                                                  self.info["natoms"])
         else:
             raise ValueError("Wrong atomic_number dimensions.")
+        
+        # load positions, stored as cartesion no matter what provided.
         pos = np.loadtxt(os.path.join(root, "positions.dat"))
         assert pos.shape[0] == self.info["nframes"] * self.info["natoms"]
         pos = pos.reshape(self.info["nframes"], self.info["natoms"], 3)
@@ -69,23 +81,27 @@ class _TrajData(object):
             self.data["pos"] = pos @ self.data["cell"]
         else:
             raise NameError("Position type must be cart / frac.")
-
-        if os.path.exists(os.path.join(self.root, "eigenvalues.npy")):
+        
+        # load optional data files
+        if os.path.exists(os.path.join(self.root, "eigenvalues.npy")) and get_eigenvalues==True:
             assert os.path.exists(os.path.join(self.root, "kpoints.npy"))
             kpoints = np.load(os.path.join(self.root, "kpoints.npy"))
-            if len(kpoints.shape) == 2:
+            if kpoints.ndim == 2:
                 # same kpoints, then copy it to all frames.
                 if kpoints.shape[0] == self.info["bandinfo"]["nkpoints"]:
                     kpoints = np.expand_dims(kpoints, axis=0)
                     self.data["kpoints"] = np.broadcast_to(kpoints, (self.info["nframes"], 
                                                                      self.info["bandinfo"]["nkpoints"], 3))
                 else:
-                    raise ValueError("kpoints in .npy not equal to nkpoints in bandinfo. ")
+                    raise ValueError("kpoints in `.npy` file not equal to nkpoints in bandinfo. ")
             elif atomic_numbers.shape[0] == self.info["nframes"]:
                 self.data["kpoints"] = kpoints
             else:
                 raise ValueError("Wrong kpoint dimensions.")
             eigenvalues = np.load(os.path.join(self.root, "eigenvalues.npy"))
+            # special case: trajectory contains only one frame
+            if eigenvalues.ndim == 2:
+                eigenvalues = np.expand_dims(eigenvalues, axis=0)
             assert eigenvalues.shape[0] == self.info["nframes"]
             assert eigenvalues.shape[1] == self.info["bandinfo"]["nkpoints"]
             assert eigenvalues.shape[2] == self.info["bandinfo"]["nbands"]
@@ -93,10 +109,54 @@ class _TrajData(object):
             #self.data["eigenvalues"] = eigenvalues.reshape(self.info["nframes"], 
             #                                               self.info["bandinfo"]["nkpoints"], 
             #                                               self.info["bandinfo"]["nbands"])            
-        if os.path.exists(os.path.join(self.root, "hamiltonians.h5")):
+        if os.path.exists(os.path.join(self.root, "hamiltonians.h5")) and get_Hamiltonian==True:
             self.data["hamiltonian_blocks"] = h5py.File(os.path.join(self.root, "hamiltonians.h5"), "r")
-        if os.path.exists(os.path.join(self.root, "overlaps.h5")):
-            self.data["overlap_blocks"] = h5py.File(os.path.join(self.root, "overlaps.h5"), "r")
+            if os.path.exists(os.path.join(self.root, "overlaps.h5")):
+                self.data["overlap_blocks"] = h5py.File(os.path.join(self.root, "overlaps.h5"), "r")
+        
+        # this is used to clear the tmp files to load ase trajectory only.
+        if _clear:
+            os.remove(os.path.join(root, "positions.dat"))
+            os.remove(os.path.join(root, "cell.dat"))
+            os.remove(os.path.join(root, "atomic_numbers.dat"))
+
+    @classmethod
+    def from_ase_traj(cls,
+                      root: str, 
+                      AtomicData_options: Dict[str, Any] = {},
+                      get_Hamiltonian = False,
+                      get_eigenvalues = False,
+                      info = None):
+        assert info["pos_type"] == "ase"
+        # ase use cartesian by default.
+        info["pos_type"] = "cart"
+
+        traj_file = glob.glob(f"{root}/*.traj")
+        assert len(traj_file) == 1, print("only one ase trajectory file can be provided.")
+        traj = Trajectory(traj_file[0], 'r')
+        positions = []
+        cell = []
+        atomic_numbers = []
+        for atoms in traj:
+            positions.append(atoms.get_positions())
+            cell.append(atoms.get_cell())
+            atomic_numbers.append(atoms.get_atomic_numbers())
+        positions = np.array(positions)
+        positions = positions.reshape(-1, 3)
+        cell = np.array(cell)
+        cell = cell.reshape(-1, 3)
+        atomic_numbers = np.array(atomic_numbers)
+        atomic_numbers = atomic_numbers.reshape(-1, 1)
+        np.savetxt(os.path.join(root, "positions.dat"), positions)
+        np.savetxt(os.path.join(root, "cell.dat"), cell)
+        np.savetxt(os.path.join(root, "atomic_numbers.dat"), atomic_numbers, fmt='%d')
+
+        return cls(root=root,
+                   AtomicData_options=AtomicData_options,
+                   get_Hamiltonian=get_Hamiltonian,
+                   get_eigenvalues=get_eigenvalues,
+                   info=info,
+                   _clear=True)
         
     def toAtomicDataList(self, idp: TypeMapper = None):
         data_list = []
@@ -108,13 +168,13 @@ class _TrajData(object):
                 pbc = self.info["pbc"],
                 **self.AtomicData_options)
             if "hamiltonian_blocks" in self.data:
-                assert idp is not None, "LCAO Basis must be provided for loading Hamiltonian."
+                assert idp is not None, "LCAO Basis must be provided  in `common_option` for loading Hamiltonian."
                 if "overlap_blocks" not in self.data:
                     self.data["overlap_blocks"] = False
                 # e3 = E3Hamiltonian(idp=idp, decompose=True)
                 ham_block_to_feature(atomic_data, idp, 
-                                     self.data["hamiltonian_blocks"][str(frame)], 
-                                     self.data["overlap_blocks"][str(frame)])
+                                     self.data["hamiltonian_blocks"][str(frame+1)], 
+                                     self.data["overlap_blocks"][str(frame+1)])
                 # with torch.no_grad():
                 #     atomic_data = e3(atomic_data.to_dict())
                 # atomic_data = AtomicData.from_dict(atomic_data)
@@ -147,7 +207,12 @@ class DefaultDataset(AtomicInMemoryDataset):
             AtomicData_options: Dict[str, Any] = {},
             include_frames: Optional[List[int]] = None,
             type_mapper: TypeMapper = None,
+            get_Hamiltonian: bool = False,
+            get_eigenvalues: bool = False
     ):
+        self.root = root
+        self.url = url
+
         self.file_name = []
         for dir_name in os.listdir(root):
             if os.path.isdir(os.path.join(root, dir_name)):
@@ -156,17 +221,36 @@ class DefaultDataset(AtomicInMemoryDataset):
                         self.file_name.append(dir_name)
                 else:
                     self.file_name.append(dir_name)
-        # the type_mapper must be stored here in order to load Hamiltonian.
-        #all_basis = []
-        #for file in self.file_name:
-        #    file_info = j_loader(os.path.join(file, "info.json"))
-        #    all_basis.append(file_info["basis"])
-        #sort_basis = {}
-        #for basis in all_basis:
-        #    for symbol, orbitals in basis.items():
-        #        if symbol not in sort_basis:
-        #            sort_basis[symbol] = orbitals
-        #type_mapper = OrbitalMapper(sort_basis)
+        # load all data files            
+        self.raw_data = []
+
+        # load a public info.json if provided.
+        if "info.json" in os.listdir(root):
+            info = j_loader(os.path.join(root, "info.json"))
+        else:
+            # load seperately.
+            info = None
+
+        for file in self.file_name:
+            if info is None:
+                # use info provided in this trajectory.
+                try: info = j_loader(os.path.join(root, file, "info.json"))
+                except Exception as e:
+                    raise Exception(f"info.json is not properly provided for `{file}`.") from e
+            if info["pos_type"] == "ase":
+                subdata = _TrajData.from_ase_traj(os.path.join(self.root, file), 
+                                AtomicData_options,
+                                get_Hamiltonian, 
+                                get_eigenvalues,
+                                info=info)
+            else:
+                subdata = _TrajData(os.path.join(self.root, file), 
+                                AtomicData_options,
+                                get_Hamiltonian, 
+                                get_eigenvalues,
+                                info=info)
+            self.raw_data.append(subdata)
+        
         super().__init__(
             file_name=self.file_name,
             url=url,
@@ -176,17 +260,11 @@ class DefaultDataset(AtomicInMemoryDataset):
             type_mapper=type_mapper,
         )
 
-    def setup_data(self):
-        self.data = []
-        for file in self.file_name:
-            subdata = _TrajData(os.path.join(self.root, file), self.AtomicData_options)
-            self.data.append(subdata)
-
     def get_data(self):
-        self.setup_data()
         all_data = []
-        for subdata in self.data:
-            # the type_mapper here is loaded in `dataset` type as `transform` attritube
+        for subdata in self.raw_data:
+            # the type_mapper here is loaded in PyG `dataset` type as `transform` attritube
+            # so the OrbitalMapper can be accessed by self.transform here
             subdata_list = subdata.toAtomicDataList(self.transform)
             all_data += subdata_list
         return all_data
--- dptb/entrypoints/data.py
+++ dptb/entrypoints/data.py
@@ -15,7 +15,7 @@ def data(
     # ABACUS parsing input like:
     # {  "type": "ABACUS",
     #    "parse_arguments": {
-    #        "input_dir": "alice/bob",
+    #        "input_path": "alice_*/*_bob/system_No_*",
     #        "preprocess_dir": "charlie/david",
     #        "only_overlap": false, 
     #        "get_Hamiltonian": true, 
@@ -23,17 +23,17 @@ def data(
     #        "get_eigenvalues": true } }
     if jdata["type"] == "ABACUS":
         abacus_args = jdata["parse_arguments"]
-        assert abacus_args.get("input_dir") is not None, "ABACUS calculation results MUST be provided."
+        assert abacus_args.get("input_path") is not None, "ABACUS calculation results MUST be provided."
         assert abacus_args.get("preprocess_dir") is not None, "Please assign a dictionary to store preprocess files."
 
         print("Begin parsing ABACUS output...")
-        h5_filenames = recursive_parse(**abacus_args)
+        recursive_parse(**abacus_args)
         print("Finished parsing ABACUS output.")
         
-        # write all h5 files to be used in building AtomicData
-        with open(os.path.join(abacus_args["preprocess_dir"], "AtomicData_file.txt"), "w") as f:
-            for filename in h5_filenames:
-                f.write(filename + "\n")
+        ## write all h5 files to be used in building AtomicData
+        #with open(os.path.join(abacus_args["preprocess_dir"], "AtomicData_file.txt"), "w") as f:
+        #    for filename in h5_filenames:
+        #        f.write(filename + "\n")
 
     else:
         raise Exception("Not supported software output.")
